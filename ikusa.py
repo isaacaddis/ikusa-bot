8d3c94ac3eaf352d3de96817ba06d7441b91a942
Isaac Addis <isaacaddis21@gmail.com>
2019-08-07 14:50:36 -0700

    interactive config mode and custom help command

--- ikusa.py
+++ ikusa.py
@@ -19,6 +19,7 @@ from helper import Helper
 prefix = "."
 CHANNEL_ID = 608048185037946883
 bot = commands.Bot(command_prefix=prefix, description='Discord group event scheduler')
+bot.remove_command("help")
 h = Helper()
 
 '''
@@ -41,6 +42,32 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(bot.latency)
 
+@bot.event
+async def on_message(message):
+    if message.content.startswith('ikusa config'):
+        channel = message.channel
+        author = message.author
+        def check(ms):
+            return (ms.channel == channel and ms.author == author)
+        await message.channel.send("I'm here! Start off by telling me what date your event will take place. (YY-MM-DD)")
+        date = (await bot.wait_for("message",check=check)).content
+        await channel.send("Okay, I've got you scheduled for {0}".format(date))
+        await channel.send("Next, tell me the name of your event.")
+        event = (await bot.wait_for("message",check=check)).content
+        await channel.send("Event name '{0}'? Got it.".format(event))
+        await channel.send("Now, tell me what time the event will take place. (HH:MM:SS)")
+        t = (await bot.wait_for("message",check=check)).content
+        await channel.send("What timezone is {0} configured for? (Example: GMT-7)".format(t))
+        zone = (await bot.wait_for("message",check=check)).content
+        await channel.send("Lastly, tell me who you want to send this to. You can get the ID of a role or channel by entering '\@role' or '\#channel' (without the quotes).")
+        role = (await bot.wait_for("message",check=check)).content
+        sql = "INSERT INTO calendar (d, event, t, zone, role) VALUES (%s, %s, %s, %s, %s)"
+        vals = (date, event, t, zone, role)
+        cursor.execute(sql,vals)
+        embed = Embed(title = "Ikusa [Bot]", color=0xe87400)
+        embed.add_field(name="All set!", value = "You chose to make reservation '{0}' at {1} for {2}     in timezone {3} to members of {4}".format(event, date, t, zone, role))
+        msg = await channel.send(embed=embed)
+
 '''
     Main command
     Params: date (YY-MM-DD) (str), event (str), t (str), zone (str), role (str)
@@ -58,7 +85,6 @@ async def start(ctx, date, event, t, zone, role):
     embed = Embed(title = "Ikusa [Bot]", color=0xe87400)
     embed.add_field(name="All set!", value = "You chose to make reservation '{0}' at {1} for {2}     in timezone {3} to members of {4}".format(event, date, t, zone, role))
     msg = await channel.send(embed=embed)
-    #msg = await channel.send("All set! :grinning: You chose to make reservation '{0}' at {1} for {2} in timezone {3} to members of {4}".format(event, date, t, zone, role))
     await msg.add_reaction(emoji="\u2705")
 
 '''
@@ -88,6 +114,19 @@ async def background_loop():
         await channel.send("You have no events planned within the next three days. TODO: Delete this in production, it would be annoying.")
     '''
     asyncio.sleep(86400)
+
+'''
+    Custom help command
+'''
+
+@bot.command(pass_context=True)
+async def help(ctx):
+    embed = Embed(title = "Ikusa [Bot]", color=0xe87400)
+    embed.add_field(name="Usage", value="You can use Ikusa with one .start command or by sending 'ikusa config' (without quotes)")
+    embed.add_field(name=".start", value="Call .start like this: .start 2019-08-12 hangout 12:30:00 GMT-7 Splatoon2. This will mention users with role or in channel 'Splatoon2' for event 'hangout' for August 12, 2019 at 12:30. You MUST use this format or your scheduling will not work correctly. ")
+    embed.add_field(name="ikusa config", value="This method will require you to input the data as shown above, but in a step-by-step manner with directions, as opposed to a single-step command. You can also use multi-word event names with this method (commands treat space-delimited words as individual arguments)")
+    await ctx.send(embed=embed)
+    
 
 bot.loop.create_task(background_loop()) #initializes background loop as task
 bot.run("NjA1NTc3ODMyMTAxNDQ1NzEx.XUEV0w.RV3A5DDWjZDJzknoFoX6L0CJd0k")
